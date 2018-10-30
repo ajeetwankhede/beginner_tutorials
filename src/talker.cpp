7f88b7026e40b7d9750ddc41d8b3ae04231b2164
ajeetwankhede <ajeet@umd.edu>
2018-10-30 02:06:18 -0400

    Correct errors as per cpplint.

--- src/talker.cpp
+++ src/talker.cpp
@@ -29,17 +29,14 @@
  *
  *  @section DESCRIPTION
  *
- *  Talker for Beginner tutorial for creating a ROS package to publish custom string message
+ *  Beginner tutorial for creating a ROS package to publish custom string message
+ *  This tutorial demonstrates simple sending of messages over the ROS system.
  */
 #include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   /**
    * The ros::init() function needs to see argc and argv so that it can perform
    * any ROS arguments and name remapping that were provided at the command line.
@@ -85,15 +82,14 @@ int main(int argc, char **argv)
    * a unique string for each message.
    */
   int count = 0;
-  while (ros::ok())
-  {
+  while (ros::ok()) {
     /**
      * This is a message object. You stuff it with data, and then publish it.
      */
     std_msgs::String msg;
 
     std::stringstream ss;
-    ss << "hello world " << count;
+    ss << "First ROS package " << count;
     msg.data = ss.str();
 
     ROS_INFO("%s", msg.data.c_str());
